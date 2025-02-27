6bf7b6f9205f47e90b345641c331cc9d91423954
nikhmatul hidayah <hinikhmatulhidayah@gmail.com>
2025-02-27 21:07:04 +0700

    hahahihi

--- resources/views/product/all.blade.php
+++ resources/views/product/all.blade.php
@@ -10,8 +10,8 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </head>
-<body>
-<nav class="navbar navbar-expand-lg" style="background-color:#F5D5D7">
+<body class="page-product-all">
+<nav class="navbar navbar-expand-lg" style="background-color:#F5D5D7; border-style:solid; border-width:0px 0px 1px 0px">
 <div class="container-fluid">
     <a class="navbar-brand" href="/product/all">
       <img src="{{ asset('asset/icon.png')}}" alt="Logo" width="100" height="50">
