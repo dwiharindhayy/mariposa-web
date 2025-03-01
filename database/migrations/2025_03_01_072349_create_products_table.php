54376ca991cb4e3b70b6bf1772a921af4cec204d
nikhmatul hidayah <hinikhmatulhidayah@gmail.com>
2025-03-01 14:27:50 +0700

    add migration

--- database/migrations/2025_03_01_072349_create_products_table.php
+++ database/migrations/2025_03_01_072349_create_products_table.php
@@ -11,10 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id_product');
+            $table->string('name');
+            $table->string('image');
+            $table->string('size');
+            $table->string('category');
+            $table->integer('price');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('products');
     }
 };
